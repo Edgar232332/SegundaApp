f1cf1f6393d7d558b9a64c726ccf5d6933ef3983
unknown <edgardiazoficial11@gmail.com>
2025-04-11 14:57:40 -0500

    Hola

--- app.py
+++ app.py
@@ -12,19 +12,21 @@ tab1, tab2 = st.tabs(["Tab 1", "Tab 2"])
 with tab1:
     fig, ax = plt.subplots(1, 3, figsize = (10,4))
     tab_freq = ran["educ"].value_counts().sort_index() 
-    ax[0].bar(tab_freq.index, tab_freq.values)
-    ax[1].hist(ran["edad"], bins = 30)
-    ax[2].hist(ran["wage"], bins = 40) 
+    ax[0].bar(tab_freq.index, tab_freq.values, color = "black")
+    ax[1].hist(ran["edad"], bins = 30, color = "green")
+    ax[2].hist(ran["wage"], bins = 40, color = "pink") 
     st.pyplot(fig)
 
     fig, ax = plt.subplots(1, 2, figsize = (10,4)) 
-    ax[0].scatter(ran["educ"], ran["wage"])
-    ax[1].scatter(ran["edad"], ran["wage"])
+    ax[0].scatter(ran["educ"], ran["wage"], color = "red")
+    ax[1].scatter(ran["edad"], ran["wage"], color = "orange")
     st.pyplot(fig)
 with tab2:
     fig = px.treemap(data_frame = apgn,
            path = [px.Constant("PGN"),
                    "Nombre Sector",
                    "Tipo de gasto"],
-           values = "Valor")
+           values = "Valor",
+           color = "Valor",
+           color_continuous_scale = "Viridis" )
     st.plotly_chart(fig)
